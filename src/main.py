75da4336318e96619c3245d50b009251748aff4b
Prince Maphupha <princemaphupha@Princes-Air.home>
2024-12-03 20:19:31 +0200

    Chore: Added a train class, Feature: Added joystick functionality

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import pygame
-
 from utilities import *
+from train import *
  
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GRASSGREEN = (0, 255, 100)
  
-# initialize pygame
+# Initialize pygame
 pygame.init()
 
 SCREEN_WIDTH = 1000
@@ -15,41 +15,61 @@ SCREEN_HEIGHT = 700
 
 screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
  
-# create a window
+# Create a window
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Train Simulator - 2D")
 
-track_width = 60
-track_rail = 3
+track_width = 75
+track_rail = 4
 
-train_width = track_width + (2* track_rail)
+train_width = track_width + (2 * track_rail)
+train_height = (train_width / 343) * 700
 
-train_img = loadImage("train.png", train_width, (train_width / 343) * 700)
- 
-# clock is used to set a max fps
+train = Train(500, 500, track_width, train_height)
+
+
+
+train_img = loadImage("train.png", train.w, train.h)
+
+# Initialize joystick
+pygame.joystick.init()
+joystick = None
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+# Clock is used to set a max fps
 clock = pygame.time.Clock()
 FPS = 60
 
- 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-     
-    #clear the screen
+    
+    # Joystick controls
+    if joystick:
+        # Axis 0 (horizontal movement)
+        xAxis = joystick.get_axis(0)  # Returns a float between -1.0 and 1.0
+        yAxis = joystick.get_axis(1)
+        train.y += yAxis * 5  # Adjust speed as needed
+
+
+    
+    # Clear the screen
     screen.fill(GRASSGREEN)
     
     pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH - (track_width + (track_rail * 2))) / 2, 0), ((SCREEN_WIDTH - (track_width + (track_rail * 2))) / 2, SCREEN_HEIGHT), track_rail)
     pygame.draw.line(screen, BLACK, (((SCREEN_WIDTH - (track_width + (track_rail * 2))) / 2) + track_width, 0), (((SCREEN_WIDTH - (track_width + (track_rail * 2))) / 2) + track_width, SCREEN_HEIGHT), track_rail)
 
-    # flip() updates the screen to make our changes visible
-
-    screen.blit(train_img, ((SCREEN_WIDTH - (track_width + (track_rail * 2))) / 2, 500))
+    # Draw the train
+    screen.blit(train_img, (train.x, train.y))
     
+    # Flip the display
     pygame.display.flip()
      
-    # how many updates per second
+    # How many updates per second
     clock.tick(FPS)
  
 pygame.quit()
---
+++ src/train.py
@@ -0,0 +1,6 @@
+class Train:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
